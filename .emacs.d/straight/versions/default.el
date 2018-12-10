bb3cc27241c9890c638887cfd13d0c132ee35af6
ttsutchi <tailegog@gmail.com>
2018-12-10 22:13:14 +0900

    [Add] add .emacs.d

---
+++ .emacs.d/straight/versions/default.el
@@ -0,0 +1,7 @@
+(("company-mode" . "5adb0be1dfa1c9767e888eee5892a10a1f81d582")
+ ("elpa" . "8309dc86c5ca0d11be3620e908bf157422654627")
+ ("epkgs" . "4c871dcbb82ccdce42a2d605c19d407c125cb3c0")
+ ("melpa" . "f50b1e03dec8cb581e869a5a5f1c9f1738725cd0")
+ ("straight.el" . "3162136026660f3c9ab9e96a3a81b2a99f2612c0")
+ ("use-package" . "39a8b8812c2c9f6f0b299e6a04e504ef393694ce"))
+:saturn
