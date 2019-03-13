c4ffcc6300e68509ac48e96915122502ee8bd932
ttsutchi <tailegog@gmail.com>
2019-03-13 19:13:25 +0900

    [Add] create bin/gi.sh (not tried yet) and link to dotfiles

---
+++ bin/gi.sh
@@ -0,0 +1,52 @@
+#! /bin/sh
+
+function usage() {
+    cat <<EOM
+Usage: $0 [-p] tool
+
+tool   Tool you use in the project (AppCode, PyCharm, and so on).
+-p     Create a private repository.
+
+EOM
+
+    exit 1
+}
+
+while getopts ad:h OPT
+do
+    case $OPT in
+        p) FLAG_P=1
+           ;;
+        h) usage
+           ;;
+    esac
+done
+
+CURRENT_DIR=$(pwd)
+DIR_NAME=$(basename $CURRENT_DIR)
+
+TOOL=$1
+
+touch README.md
+echo "# $DIR_NAME" >> README.md
+
+cp ~/gitignores/$TOOL/.gitignore $CURRENT_DIR/.gitignore
+
+git init
+git add README.md .gitignore
+git commit -m "\[Add] create README.md and .gitignore"
+
+if [$FLAGP == 1]
+then
+    hub create -p $DIR_NAME
+else
+    hub create $DIR_NAME
+fi
+
+wait $!
+
+git remote add origin git@github.com:ttsutchi/$DIR_NAME.git
+git push -u origin master
+
+echo "Git repository is initialized."
+
