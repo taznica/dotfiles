58cde23ae2614620708ef9a5665e94743b1e6bce
taznica <tailegog@gmail.com>
2022-04-02 16:53:46 +0900

    add: .emacs.d

---
+++ .emacs.d/straight/versions/default.el
@@ -0,0 +1,8 @@
+(("company-mode" . "073aef72ddf93f897a856c246c58dcdfe003674e")
+ ("el-get" . "9353309744e4f8a7c9b1adf22ec99536fb2146b0")
+ ("emacsmirror-mirror" . "d733ec0b70803781280e12f7a683ce8850c23e78")
+ ("gnu-elpa-mirror" . "bc03f8141c285538418daeff450f67d90ead2403")
+ ("melpa" . "780e7b4449c54f6ff5716b4953c85accfb7f0ebd")
+ ("straight.el" . "af5437f2afd00936c883124d6d3098721c2d306c")
+ ("use-package" . "a7422fb8ab1baee19adb2717b5b47b9c3812a84c"))
+:beta
